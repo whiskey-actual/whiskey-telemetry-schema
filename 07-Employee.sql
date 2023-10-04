ab8c11150c1e4befc48b917991a18931d303cb94
whiskey-actual <actualwhiskey@gmail.com>
2023-10-04 16:59:45 -0400

    add AD user data query

--- 07-Employee.sql
+++ 07-Employee.sql
@@ -1,25 +1,25 @@
 CREATE TABLE [dbo].[Employee] (
     [EmployeeID]                                INT             NOT NULL    IDENTITY(1,1),  
     EmployeeActiveDirectoryDN                   VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryCN                   VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectorySN                   VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryCountry              VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryCity                 VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryState                VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryTitle                VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryOffice               VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryTelephoneNumber      VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryGivenName            VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryDisplayName          VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryDepartment           VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryStreetAddress        VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryUserName             VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryEmployeeID           VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectorySAMAccountName       VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryPrincipalName        VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryUserMail             VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryLogonCount           INT             NOT NULL,
-    EmployeeActiveDirectoryCreatedDate          DATETIME2       NOT NULL,
+    EmployeeActiveDirectoryCN                   VARCHAR(255)    NULL,
+    EmployeeActiveDirectorySN                   VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryCountry              VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryCity                 VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryState                VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryTitle                VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryOffice               VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryTelephoneNumber      VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryGivenName            VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryDisplayName          VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryDepartment           VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryStreetAddress        VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryUserName             VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryEmployeeID           VARCHAR(255)    NULL,
+    EmployeeActiveDirectorySAMAccountName       VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryPrincipalName        VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryUserMail             VARCHAR(255)    NULL,
+    EmployeeActiveDirectoryLogonCount           INT             NULL,
+    EmployeeActiveDirectoryCreatedDate          DATETIME2       NULL,
     EmployeeActiveDirectoryChangedDate          DATETIME2       NULL,
     EmployeeActiveDirectoryBadPasswordTime      DATETIME2       NULL,
     EmployeeActiveDirectoryLastLogon            DATETIME2       NULL,
@@ -32,6 +32,6 @@ CREATE UNIQUE NONCLUSTERED INDEX IDX_Employee_EmployeeActiveDirectoryDN ON Emplo
 GO
 
 SET IDENTITY_INSERT Employee ON
-INSERT INTO Employee(EmployeeID, EmployeeEmailAddress) VALUES (0, 'UNKNOWN')
+INSERT INTO Employee(EmployeeID, EmployeeActiveDirectoryDN) VALUES (0, 'UNKNOWN')
 SET IDENTITY_INSERT Employee OFF
 GO
