23240ffb67dc356864d52a8dbffc3803506b4cad
whiskey-actual <actualwhiskey@gmail.com>
2023-10-04 08:52:28 -0400

    add os variant & version

--- 07-OperatingSystem.sql
+++ 07-OperatingSystem.sql
@@ -2,11 +2,6 @@
 CREATE TABLE [dbo].[OperatingSystem] (
     [OperatingSystemID]			            INT                 NOT NULL    IDENTITY(1,1),
     [OperatingSystemDescription]            VARCHAR(255)        NOT NULL,
-    [OperatingSystemVersionMajor]           INT                 NULL,
-    [OperatingSystemVersionMinor]           INT                 NULL,
-    [OperatingSystemBuild]                  INT                 NULL,
-    [OperatingSystemRevision]               INT                 NULL,
-    [OperatingSystemNormalizedID]           INT                 NULL,
     CONSTRAINT [PK_OperatingSystem] PRIMARY KEY CLUSTERED ([OperatingSystemID] ASC),
 );
 GO
@@ -15,12 +10,42 @@ GO
 SET IDENTITY_INSERT OperatingSystem ON;
 INSERT INTO OperatingSystem (OperatingSystemID, OperatingSystemDescription) VALUES (0, 'UNKNOWN')
 SET IDENTITY_INSERT OperatingSystem OFF;
+GO
 
 -- add index(es)
-CREATE UNIQUE NONCLUSTERED INDEX IDX_OperatingSystem_Complex_01 ON OperatingSystem(
-    OperatingSystemDescription,
-    OperatingSystemVersionMajor,
-    OperatingSystemVersionMinor,
-    OperatingSystemBuild
+CREATE UNIQUE NONCLUSTERED INDEX IDX_OperatingSystem_OperatingSystemDescription ON OperatingSystem(OperatingSystemDescription);
+GO
+
+
+--------------------------------------------------------------------------------------------------------------
+CREATE TABLE [dbo].[OperatingSystemVariant] (
+    [OperatingSystemVariantID]              INT                 NOT NULL    IDENTITY(1,1),
+    [OperatingSystemVariantDescription]     VARCHAR(255)        NOT NULL,
+    CONSTRAINT [PK_OperatingSystemVariant] PRIMARY KEY CLUSTERED ([OperatingSystemVariantID] ASC)
 );
 GO
+
+SET IDENTITY_INSERT OperatingSystemVariant ON;
+INSERT INTO OperatingSystemVariant (OperatingSystemVarianrID, OperatingSystemVariantDescription) VALUES (0, 'UNKNOWN')
+SET IDENTITY_INSERT OperatingSystemVariant OFF;
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IDX_OperatingSystemVariant_OperatingSystemVariantDescription ON OperatingSystemVariant(OperatingSystemVariantDescription);
+GO
+
+--------------------------------------------------------------------------------------------------------------
+CREATE TABLE [dbo].[OperatingSystemVersion] (
+    [OperatingSystemVersionID]              INT                 NOT NULL    IDENTITY(1,1),
+    [OperatingSystemVersionDescription]     VARCHAR(255)        NOT NULL,
+    CONSTRAINT [PK_OperatingSystemVersion] PRIMARY KEY CLUSTERED ([OperatingSystemVersionID] ASC)
+);
+GO
+
+SET IDENTITY_INSERT OperatingSystemVersion ON;
+INSERT INTO OperatingSystemVersion (OperatingSystemVarianrID, OperatingSystemVersionDescription) VALUES (0, 'UNKNOWN')
+SET IDENTITY_INSERT OperatingSystemVersion OFF;
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IDX_OperatingSystemVersion_OperatingSystemVersionDescription ON OperatingSystemVersion(OperatingSystemVersionDescription);
+GO
+
