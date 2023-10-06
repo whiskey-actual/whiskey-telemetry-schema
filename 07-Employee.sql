1557fe4f896c9108d3f84510f171a9dab47ff8db
whiskey-actual <actualwhiskey@gmail.com>
2023-10-06 09:56:13 -0400

    add AAD user data

--- 07-Employee.sql
+++ 07-Employee.sql
@@ -1,38 +1,81 @@
 CREATE TABLE [dbo].[Employee] (
-    [EmployeeID]                                INT             NOT NULL    IDENTITY(1,1),  
-    EmployeeActiveDirectoryDN                   VARCHAR(255)    NOT NULL,
-    EmployeeActiveDirectoryCN                   VARCHAR(255)    NULL,
-    EmployeeActiveDirectorySN                   VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryCountry              VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryCity                 VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryState                VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryTitle                VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryOffice               VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryTelephoneNumber      VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryGivenName            VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryDisplayName          VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryDepartment           VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryStreetAddress        VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryUserName             VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryEmployeeID           VARCHAR(255)    NULL,
-    EmployeeActiveDirectorySAMAccountName       VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryPrincipalName        VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryUserMail             VARCHAR(255)    NULL,
-    EmployeeActiveDirectoryLogonCount           INT             NULL,
-    EmployeeActiveDirectoryCreatedDate          DATETIME2       NULL,
-    EmployeeActiveDirectoryChangedDate          DATETIME2       NULL,
-    EmployeeActiveDirectoryBadPasswordTime      DATETIME2       NULL,
-    EmployeeActiveDirectoryLastLogon            DATETIME2       NULL,
-    EmployeeActiveDirectoryLastLogonTimestamp   DATETIME2       NULL,
-    EmployeeActiveDirectoryLastSeen             DATETIME2       NULL,
+    EmployeeID                          INT             NOT NULL    IDENTITY(1,1),  
+    
+    EmployeeEmailAddress                VARCHAR(255)    NOT NULL,
+    
+    ad_DN                               VARCHAR(255)    NULL,
+    ad_CN                               VARCHAR(255)    NULL,
+    ad_SN                               VARCHAR(255)    NULL,
+    ad_Country                          VARCHAR(255)    NULL,
+    ad_City                             VARCHAR(255)    NULL,
+    ad_State                            VARCHAR(255)    NULL,
+    ad_Title                            VARCHAR(255)    NULL,
+    ad_Office                           VARCHAR(255)    NULL,
+    ad_TelephoneNumber                  VARCHAR(255)    NULL,
+    ad_GivenName                        VARCHAR(255)    NULL,
+    ad_DisplayName                      VARCHAR(255)    NULL,
+    ad_Department                       VARCHAR(255)    NULL,
+    ad_StreetAddress                    VARCHAR(255)    NULL,
+    ad_UserName                         VARCHAR(255)    NULL,
+    ad_EmployeeID                       VARCHAR(255)    NULL,
+    ad_SAMAccountName                   VARCHAR(255)    NULL,
+    ad_PrincipalName                    VARCHAR(255)    NULL,
+    ad_UserMail                         VARCHAR(255)    NULL,
+    ad_LogonCount                       INT             NULL,
+    ad_CreatedDate                      DATETIME2       NULL,
+    ad_ChangedDate                      DATETIME2       NULL,
+    ad_BadPasswordTime                  DATETIME2       NULL,
+    ad_LastLogon                        DATETIME2       NULL,
+    ad_LastLogonTimestamp               DATETIME2       NULL,
+    ad_LastSeen                         DATETIME2       NULL,
+
+    aad_UserPrincipalName               VARCHAR(255)    NULL,
+    aad_Id                              VARCHAR(255)    NULL,
+    aad_BusinessPhone                   VARCHAR(255)    NULL,
+    aad_BisplayName                     VARCHAR(255)    NULL,
+    aad_GivenName                       VARCHAR(255)    NULL,
+    aad_JobTitle                        VARCHAR(255)    NULL,
+    aad_MobilePhone                     VARCHAR(255)    NULL,
+    aad_OfficeLocation                  VARCHAR(255)    NULL,
+    aad_Surname                         VARCHAR(255)    NULL,
+    aad_AccountEnabled                  VARCHAR(255)    NULL,
+    aad_AssignedPlans                   VARCHAR(255)    NULL,
+    aad_City                            VARCHAR(255)    NULL,
+    aad_Country                         VARCHAR(255)    NULL,
+    aad_CreationType                    VARCHAR(255)    NULL,
+    aad_Department                      VARCHAR(255)    NULL,
+    aad_EmployeeHireDate                VARCHAR(255)    NULL,
+    aad_EmployeeId                      VARCHAR(255)    NULL,
+    aad_EmployeeOrgData                 VARCHAR(255)    NULL,
+    aad_EmployeeType                    VARCHAR(255)    NULL,
+    aad_OnPremisesDistinguishedName     VARCHAR(255)    NULL,
+    aad_OnPremisesDomainName            VARCHAR(255)    NULL,
+    aad_OnPremisesSamAccountName        VARCHAR(255)    NULL,
+    aad_OnPremisesUserPrincipalName     VARCHAR(255)    NULL,
+    aad_PasswordPolicies                VARCHAR(255)    NULL,
+    aad_PostalCode                      VARCHAR(255)    NULL,
+    aad_State                           VARCHAR(255)    NULL,
+    aad_StreetAddress                   VARCHAR(255)    NULL,
+    aad_UserType                        VARCHAR(255)    NULL,
+    aad_CreatedDateTime                 DATETIME2       NULL,
+    aad_DeletedDateTime                 DATETIME2       NULL,
+    aad_LastPasswordChangeDateTime      DATETIME2       NULL,
+
+
     CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
 )
 GO
 
-CREATE UNIQUE NONCLUSTERED INDEX IDX_Employee_EmployeeActiveDirectoryDN ON Employee(EmployeeActiveDirectoryDN)
+CREATE UNIQUE NONCLUSTERED INDEX IDX_Employee_EmployeeEmailAddress ON Employee(EmployeeEmailAddress)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IDX_Employee_ad_DN ON Employee(ad_DN) WHERE ad_DN IS NOT NULL
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IDX_Employee_ad_DN ON Employee(aad_Id) WHERE ad_DN IS NOT NULL
 GO
 
 SET IDENTITY_INSERT Employee ON
-INSERT INTO Employee(EmployeeID, EmployeeActiveDirectoryDN) VALUES (0, 'UNKNOWN')
+INSERT INTO Employee(EmployeeID, EmployeeEmailAddress) VALUES (0, 'UNKNOWN')
 SET IDENTITY_INSERT Employee OFF
 GO
